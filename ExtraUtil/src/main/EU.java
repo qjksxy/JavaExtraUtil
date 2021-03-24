dcf0197edcfd127a90eeaf4ed5adfe1d8aa37e29
QJK <qjksxy@163.com>
2021-03-24 22:42:44 +0800

    完善javadoc

--- ExtraUtil/src/main/EU.java
+++ ExtraUtil/src/main/EU.java
@@ -1,39 +1,36 @@
 package main;
 
 import java.math.BigDecimal;
-/*
+/**
  * 扩展类ExtraUtil
- * 包括改进的输出方法，产生整数数组的range方法，精确的浮点数计算方法
+ * 
+ * <p>包括改进的输出方法，产生整数数组的range方法，精确的浮点数计算方法</p>
  * @author qjk
  * @author qjksxy@163.com
  * @version 0.1.1
  */
 public class EU {
 	private EU() {}
-	/*
+	/**
 	 * 改进的输出方法，接受多个参数，对每个参数换行输出
-	 * @param 欲输出的对象或数据 
-	 * @return 无返回值
-	 * @throws 无异常
+	 * @param obj 欲输出的对象或数据 
 	 */
 	public static void println(Object... obj) {
 		for(Object o : obj) {
 			System.out.println(o);
 		}
 	}
-	/*
+	/**
 	 * 简写的输出方法
-	 * @return 无返回值
-	 * @throws 无异常
+	 * @param format 与System.out.printf方法中相同
+	 * @param obj 与System.out.printf方法中相同
 	 */
 	public static void printf(String format, Object... obj) {
 		System.out.printf(format, obj);
 	}
-	/*
+	/**
 	 * 改进的输出方法，接受多个参数，对每个参数不换行输出，并以", "间隔
 	 * @param obj 欲输出的对象或数据
-	 * @return 无返回值
-	 * @throws 无异常
 	 */
 	public static void print(Object... obj) {
 		int i;
@@ -43,11 +40,10 @@ public class EU {
 		}
 		System.out.print(obj[i]);
 	}
-	/*
+	/**
 	 * 生成一个从0开始的int数组
 	 * @param len 指定数组长度
 	 * @return 从0开始，长度为len的数组
-	 * @throws 无异常
 	 */
 	public static int[] range(int len) {
 		int[] array = new int[len];
@@ -56,7 +52,7 @@ public class EU {
 		}
 		return array;
 	}
-	/*
+	/**
 	 * 生成一个int数组
 	 * @param begin 数组起始值(包含)
 	 * @param end 数组结束值(不包含)
@@ -74,7 +70,7 @@ public class EU {
 		}
 		return array;
 	}
-	/*
+	/**
 	 * 生成一个int数组
 	 * @param begin 数组起始值(包含)
 	 * @param end 数组结束值(不包含)
@@ -94,49 +90,45 @@ public class EU {
 		}
 		return array;
 	}
-	/*
+	/**
 	 * 进行精确计算的加法方法
 	 * @param addend1 第一个加数
 	 * @param addend2 第二个加数
 	 * @return 加法运算的和
-	 * @throws 无异常
 	 */
 	public static double add(double addend1, double addend2) {
 		BigDecimal bigdecimal1 = BigDecimal.valueOf(addend1);
 		BigDecimal bigdecimal2 = BigDecimal.valueOf(addend2);
 		return bigdecimal1.add(bigdecimal2).doubleValue();
 	}
-	/*
+	/**
 	 * 进行精确计算的减法方法
 	 * @param minuend 被减数
 	 * @param subtrahend 减数
 	 * @return 减法运算的差
-	 * @throws 无异常
 	 */
 	public static double sub(double minuend, double subtrahend) {
 		BigDecimal bigdecimal1 = BigDecimal.valueOf(minuend);
 		BigDecimal bigdecimal2 = BigDecimal.valueOf(subtrahend);
 		return bigdecimal1.subtract(bigdecimal2).doubleValue();
 	}
-	/*
+	/**
 	 * 进行精确计算的乘法方法
 	 * @param multiplier1 第一个乘数
 	 * @param multiplier2 第二个乘数
 	 * @return 乘法运算的积
-	 * @throws 无异常
 	 */
 	public static double mul(double multiplier1, double multiplier2) {
 		BigDecimal bigdecimal1 = BigDecimal.valueOf(multiplier1);
 		BigDecimal bigdecimal2 = BigDecimal.valueOf(multiplier2);
 		return bigdecimal1.multiply(bigdecimal2).doubleValue();
 	}
-	/*
+	/**
 	 * 进行精确计算的除法方法
 	 * @param dividend 被除数
 	 * @param divisor 除数
 	 * @param scale 计算精度
 	 * @return 除法运算的商，末尾四舍五入
-	 * @throws 无异常
 	 */
 	public static double div(double dividend, double divisor, int scale) {
 		BigDecimal bigdecimal1 = BigDecimal.valueOf(dividend);
